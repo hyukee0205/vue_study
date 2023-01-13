894160283905819bc94bcb79ec65e7b03d2311d8
hyukee0205 <hyukee0205@gmail.com>
2023-01-13 16:04:25 +0900

    0113

--- todo_list/src/datasources/firebase.js
+++ todo_list/src/datasources/firebase.js
@@ -5,9 +5,9 @@ import 'firebase/compat/database';
 
 // 파이어베이스 DB를 초기화하고 연결
 const oDB = firebase.initializeApp({
-  //파이어베이스 콘솔에서 복사하여 붙여넣기
-  databaseURL: "https://pwa-to-do-5b211-default-rtdb.firebaseio.com",
-}).databases();
+  // 파이어베이스 콘솔에서 복사하여 붙여넣기
+  databaseURL: "https://pwa-to-do-5b211-default-rtdb.firebaseio.com/",
+}).database();
 
 // 파이어베이스 DB객체 중에서 todos 항목을 다른 곳에서 사용하도록 공개
 export const oTodosinDB = oDB.ref('todos');
